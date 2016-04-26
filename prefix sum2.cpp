da03f5bc6bf4d1012e55a5cfa16d10b88840e060
taej0127 <taej0127@naver.com>
2016-04-26 20:02:37 +0900

    1부터 시작하는 구간 합

---
+++ prefix sum2.cpp
@@ -0,0 +1,64 @@
+/* Lety */
+
+#define _CRT_SECURE_NO_WARNINGS
+
+#include		<cstdio>
+#include		<cmath>
+#include		<algorithm>
+#include		<vector>
+#include		<stack>
+#include		<queue>
+#include		<functional>
+#include		<cstring>
+#include		<string>
+#include		<map>
+#include		<set>
+#include		<iostream>
+#include		<tuple>
+
+#define ENP     printf("**Entry Point**\n")
+#define A       first
+#define B       second
+#define MP      make_pair
+#define MT      make_tuple
+#define PB      push_back
+
+using namespace std;
+
+typedef long long		 ll;
+typedef vector	<int>	 VI;
+
+int main(int argc, char **argv)
+{
+	int n;						// the # of element
+	int m;						// the # of query
+	int t;						// temp variable
+	int psum[100001]; // prefix sum
+	int a, b;					// range : [a,b]
+	scanf("%d", &n);
+	psum[0] = 0;			// psum[0] = 0 으로 초기화
+	
+	for (int i = 1; i <= n; i++) // for문 범위 주의
+	{
+		scanf("%d", &t);
+		psum[i] = psum[i - 1] + t; // calculate prefix sum
+	}
+	scanf("%d", &m);
+
+	for (int i = 0; i < m; i++)
+	{
+		scanf("%d%d", &a, &b);
+		//a--;						// starting index is 1
+		//b--;
+		printf("%d\n", psum[b] - psum[a - 1]); // !
+	}
+	return 0;
+}
+
+/* memo
+*
+*
+*
+*
+*
+*/
