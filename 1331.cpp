cae8b0f593ebd712c082f85e218397fa1c2d8296
taej0127 <taej0127@gmail.com>
2016-08-19 16:06:19 +0900

    .......

---
+++ 1331.cpp
@@ -0,0 +1,98 @@
+#include <iostream> // standard input & output cout cin
+#include <cstdio> // scanf printfsssss
+#include <vector> // std::vector
+#include <algorithm>
+#include <string> // std::string
+#include <queue> // std::queue
+#include <cstring> // memset
+#include <set> // std::set
+#include <utility> // std::pair, std::make_pair
+#include <map>
+#include <list>
+#include <stack>
+#include <deque>
+#include <cmath>
+#include <ctime>
+#include <chrono>
+#include <climits>
+#include <cassert>
+using namespace std;
+
+typedef unsigned ull;
+typedef long long ll;
+typedef vector<int> VI;
+typedef pair<int, int> pii;
+
+const char newline = '\n';
+
+const int dy[4] = {-1,0,0,1};
+const int dx[4] = {0,-1,1,0};
+#define pb(x) push_back(x)
+#define forn(i,n) for(int i = 0 ; i < int(n); ++i)
+const int mod = 1000000007;
+ll powmod(ll a, ll b) {ll res=1; a %= mod; assert(b >= 0); for(; b; b >>= 1) {if (b & 1) res = res * a % mod; a = a * a % mod;} return res;}
+
+int visited[6][6];
+
+int main(int argc, char **argv)
+{
+	ios_base::sync_with_stdio(false); cin.tie(0);
+	string s;
+	int y, x;
+	int py, px;
+	int sy, sx;
+	bool valid = true;
+	cin>>s;	
+	sy = py = s[0] - 'A';
+	sx = px = s[1] - '1';
+	visited[py][px]++;
+
+	for(int i = 0 ; i < 35 ;i++)
+	{
+		cin>>s;	
+		y = s[0] - 'A';
+		x = s[1] - '1';
+
+		if( abs(y-py) + abs(x-px) == 3 && abs(y-py) != 0 && abs(x-px) != 0)
+		{
+			// do nothing
+		}
+		else
+		{
+			valid = false;
+//			cout<<i<<newline;
+//			cout<<'('<<y<<','<<x<<'-'<<py<<','<<px<<')'<<newline;
+		
+		}
+
+		visited[y][x]++;
+		py = y;
+		px = x;
+//	cout<<y<< ' '<<x << newline;
+	}
+
+	y = sy; x = sx;
+	
+	if( abs(y-py) + abs(x-px) == 3 && abs(y-py) != 0 && abs(x-px) != 0)
+	{
+		//do nothing
+	}
+	else
+	{
+		valid = false;
+//		cout<<"here"<<newline;
+	}
+
+	for(int i =0 ; i < 6 ; i++)
+	{
+		for(int j = 0 ; j < 6 ; j++)
+		{
+			if(visited[i][j] != 1) valid = false;
+		}
+	}
+
+	if(!valid) cout<<"Invalid"<<newline;
+	else 
+		cout<<"Valid"<<newline;
+  return 0;
+}
