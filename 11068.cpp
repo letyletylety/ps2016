4b3f4328fc669ea5375c1080e2f11c5c5f73d27a
taej0127 <taej0127@gmail.com>
2016-09-01 16:16:51 +0900

    regional

---
+++ 11068.cpp
@@ -0,0 +1,70 @@
+#include <iostream> // standard input & output cout cin
+#include <cstdio> // scanf printfsssss
+#include <vector> // std::vector
+#include <algorithm>
+#include <string> // std::string
+#include <queue> // std::queue
+#include <cstring> // memset
+#include <set> // std::set
+#include <utility> // std::pair, std::make_pair
+#include <map>
+#include <list>
+#include <stack>
+#include <deque>
+#include <cmath>
+#include <ctime>
+#include <chrono>
+#include <climits>
+#include <cassert>
+using namespace std;
+
+typedef unsigned ull;
+typedef long long ll;
+typedef vector<int> VI;
+typedef pair<int, int> pii;
+
+const char newline = '\n';
+
+const int dy[4] = {-1,0,0,1};
+const int dx[4] = {0,-1,1,0};
+#define pb(x) push_back(x)
+#define forn(i,n) for(int i = 0 ; i < int(n); ++i)
+#define xx first
+#define yy second
+const int mod = 1000000007;
+const int INF = 987654321;
+ll powmod(ll a, ll b) {ll res=1; a %= mod; assert(b >= 0); for(; b; b >>= 1) {if (b & 1) res = res * a % mod; a = a * a % mod;} return res;}
+
+bool sub(int base, int n)
+{
+	vector<int> v;
+	while(n)
+	{
+		v.pb(n % base);
+		n /= base;
+	}	
+	return v == vector<int>(v.rbegin(), v.rend());
+}
+
+bool isPal(int n)
+{
+	for(int i = 2; i <= 64 ;i++)
+	{
+		if(sub(i, n)) return true;
+	}
+	return false;
+}
+
+int main(int argc, char **argv)
+{
+	ios_base::sync_with_stdio(false); cin.tie(0);
+	int n;
+	cin>>n;
+	while(n--)
+	{
+		int tt;
+		cin>>tt;
+		cout<<isPal(tt)<<newline;
+	}
+  return 0;
+}
